6d715806794a68b1bf900cc64f34f15d48250d2a
Richard Borge <richard.borge@nav.no>
2020-01-03 10:45:45 +0100

    Fikset tester

--- src/test/kotlin/no/nav/medlemskap/BostedsvilkaarTest.kt
+++ src/test/kotlin/no/nav/medlemskap/BostedsvilkaarTest.kt
@@ -1,5 +1,7 @@
 package no.nav.medlemskap
 
+import com.google.gson.GsonBuilder
+import no.nav.medlemskap.domene.Personhistorikk
 import no.nav.nare.core.evaluations.Resultat
 import org.junit.Test
 import java.time.LocalDate
@@ -7,13 +9,11 @@ import kotlin.test.assertEquals
 
 class BostedsvilkaarTest {
 
+    private val gson = GsonBuilder().registerTypeAdapter(LocalDate::class.java, localDateDeserializer).create()
+
     @Test
     fun `sjekk bostedsvillkår for gyldig person`() {
-        val testPerson = Person(Adresse(
-                landkode = "NO",
-                gyldigFra = LocalDate.now().minusMonths(20),
-                gyldigTil = null
-        ))
+        val testPerson = "enkel_bosted_norge.json".asDatagrunnlag()
 
         val evaluering = bostedsvilkaar.evaluer(testPerson)
         assertEquals(Resultat.JA, evaluering.resultat)
@@ -21,13 +21,13 @@ class BostedsvilkaarTest {
 
     @Test
     fun `sjekk bostedsvillkår for ugyldig person`() {
-        val testPerson = Person(Adresse(
-                landkode = "NO",
-                gyldigFra = LocalDate.now().minusMonths(2),
-                gyldigTil = null
-        ))
+        val testPerson = "enkel_bosted_utland.json".asDatagrunnlag()
 
         val evaluering = bostedsvilkaar.evaluer(testPerson)
         assertEquals(Resultat.NEI, evaluering.resultat)
     }
+
+    private fun String.asDatagrunnlag(): Personhistorikk =
+            gson.fromJson(BostedsvilkaarTest::class.java.getResource("/testpersoner/${this}").readText(),
+                    Personhistorikk::class.java)
 }
