22161dbcd1e1ef8dd369603b56cef8bfc82e6b60
Richard Borge <richard.borge@nav.no>
2020-01-27 10:41:16 +0100

    Legger til ekstra person i test

--- src/test/kotlin/no/nav/medlemskap/regler/personer/Persongenerator.kt
+++ src/test/kotlin/no/nav/medlemskap/regler/personer/Persongenerator.kt
@@ -21,6 +21,39 @@ val enkelNorsk = Datagrunnlag(
         dokument = listOf()
 )
 
+val enkelNorskMedArbeid = Datagrunnlag(
+        soknadsperiode = Periode(LocalDate.now(), LocalDate.now()),
+        soknadstidspunkt = LocalDate.now(),
+        brukerinput = Brukerinput(false),
+        personhistorikk = Personhistorikk(
+                statsborgerskap = listOf(Statsborgerskap("NOR", LocalDate.now(), LocalDate.now())),
+                personstatuser = listOf(),
+                bostedsadresser = listOf(),
+                postadresser = listOf(),
+                midlertidigAdresser = listOf()
+        ),
+        medlemskapsunntak = listOf(),
+        arbeidsforhold = listOf(
+                Arbeidsforhold(
+                        arbeidsavtaler = listOf(
+                                Arbeidsavtale(
+                                        periode = Periode(LocalDate.now(), LocalDate.now()),
+                                        yrkeskode = "0001",
+                                        skipsregister = Skipsregister.nor,
+                                        stillingsprosent = 25.0
+                                        )
+                        ),
+                        arbeidsfolholdstype = Arbeidsforholdstype.MARITIM,
+                        arbeidsgiver = Arbeidsgiver("Organisasjon", "1", "NOR"),
+                        utenlandsopphold = listOf(),
+                        periode = Periode(LocalDate.now(), LocalDate.now())
+                )
+        ),
+        inntekt = listOf(),
+        oppgaver = listOf(),
+        dokument = listOf()
+)
+
 val enkelAmerikansk = Datagrunnlag(
         soknadsperiode = Periode(LocalDate.now(), LocalDate.now()),
         soknadstidspunkt = LocalDate.now(),
